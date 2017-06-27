0a6fd34b5ffb5043a4a875e61d76294e9ce342a5
jamesball27 <jwrb27@gmail.com>
2017-06-27 15:33:23 -0400

    Add multipage scrolling and dynamic arrow rendering

--- lib/components/tabs_header.jsx
+++ lib/components/tabs_header.jsx
@@ -4,11 +4,32 @@ class TabsHeader extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { scrollPosition: "left" };
-    this.tabCount = this.props.tabs.length;
+    this.state = {
+      pageNumber: 1,
+      pageCount: 1,
+    };
+
     this.handleClick = this.handleClick.bind(this);
     this.scrollLeft = this.scrollLeft.bind(this);
     this.scrollRight = this.scrollRight.bind(this);
+    this.renderLeftArrow = this.renderLeftArrow.bind(this);
+    this.renderRightArrow = this.renderRightArrow.bind(this);
+  }
+
+  componentDidMount() {
+    const ulWidth = this.ul.offsetWidth;
+    const scrollWidth = this.ul.scrollWidth;
+    const pageCount = Math.ceil(scrollWidth / ulWidth);
+    const scrollAmount = Math.floor(scrollWidth / pageCount);
+    const scrollMax = scrollWidth - ulWidth;
+
+    this.setState({
+      ulWidth,
+      scrollWidth,
+      pageCount,
+      scrollAmount,
+      scrollMax
+    });
   }
 
   handleClick(tabId) {
@@ -30,40 +51,50 @@ class TabsHeader extends React.Component {
     this.setScrollInterval('right');
   }
 
-  // this only works if there are two 'pages' of tabs
-  // if there are more than two pages it will skip any in between
   setScrollInterval(direction) {
-    let bound, scroll;
+    const { scrollAmount, scrollMax } = this.state;
+
+    let bound, scroll, pageNumber;
     if (direction === 'right') {
-      bound = this.ul.scrollWidth - this.ul.offsetWidth;
-      scroll = () => { this.ul.scrollLeft += 4; };
+      bound = this.ul.scrollLeft + scrollAmount;
+      if (bound > scrollMax) { bound = scrollMax; }
+      scroll = () => { this.ul.scrollLeft += 1; };
+      pageNumber = this.state.pageNumber + 1;
     } else {
-      bound = 0;
-      scroll = () => { this.ul.scrollLeft -= 4; };
+      bound = this.ul.scrollLeft - scrollAmount;
+      if (bound < 0) { bound = 0; }
+      scroll = () => { this.ul.scrollLeft -= 1; };
+      pageNumber = this.state.pageNumber - 1;
     }
 
     const interval = setInterval(() => {
       if (this.ul.scrollLeft === bound) {
         clearInterval(interval);
-        this.setState({ scrollPosition: direction });
+        this.setState({ pageNumber });
       } else {
         scroll();
+        console.log(this.ul.scrollLeft);
       }
     }, 1);
   }
 
-  render() {
-    let arrow;
-    if (this.state.scrollPosition === 'right') {
-      arrow = <span onClick={ this.scrollLeft } className="arrow left">❰</span>;
-    } else {
-      arrow = <span onClick={ this.scrollRight } className="arrow right">❱</span>;
+  renderRightArrow() {
+    if (this.state.pageNumber < this.state.pageCount) {
+      return <span onClick={ this.scrollRight } className="arrow right">❱</span>;
     }
+  }
 
+  renderLeftArrow() {
+    if (this.state.pageNumber > 1) {
+      return <span onClick={ this.scrollLeft } className="arrow left">❰</span>;
+    }
+  }
+
+  render() {
     return(
       <div className="tabs-header-wrapper">
-        { arrow }
-        <ul className="tabs-header" ref={ ul => {this.ul = ul;}}>
+        { this.renderLeftArrow() }
+        <ul className="tabs-header" ref={ ul => {this.ul = ul;}} id="ul">
           {
             this.props.tabs.map(tab => (
               <li
@@ -76,6 +107,7 @@ class TabsHeader extends React.Component {
             ))
           }
         </ul>
+        { this.renderRightArrow() }
       </div>
     );
   }
